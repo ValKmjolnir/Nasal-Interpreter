c154d8ab491ab527ce10d80b3be6472e2f55dacc
Valk Richard Li <48872266+ValKmjolnir@users.noreply.github.com>
2019-08-07 22:04:13 +0800

    bug fix

---
+++ lab.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <thread>
+#include <cstdlib>
+#include "nasal.h"
+
+using namespace nasal;
+
+int main()
+{
+	std::cout<<">> input \"help\" to find help."<<std::endl;
+	std::string Command;
+	while(1)
+	{
+		std::cout<<">> ";
+		std::cin>>Command;
+		if(Command=="help")
+		{
+			std::cout<<">> 1.input file name to run the nasal script."<<std::endl;
+			std::cout<<">> 2.command cls to clear the screen."<<std::endl;
+			std::cout<<">> 3.command exit to shut down the program."<<std::endl;
+		}
+		else if(Command=="cls")
+			system("cls");
+		else if(Command=="exit")
+		{
+			nasal_var_stack.delete_all();
+			nasal_func_stack.delete_all();
+			break;
+		}
+		else
+		{
+			RunProcess(Command);
+			nasal_lexer.run();
+		}
+	}
+	return 0;
+}
