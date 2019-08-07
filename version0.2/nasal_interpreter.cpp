b23d6cd5d6900e374f884c8cd779032114c01389
Valk Richard Li <48872266+ValKmjolnir@users.noreply.github.com>
2019-08-08 02:25:48 +0800

    new structure

---
+++ version0.2/nasal_interpreter.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstring>
+#include "nasal.h"
+
+int main()
+{
+	
+	return 0;
+}
---
+++ version0.2/nasal_list.cpp
@@ -0,0 +1,17 @@
+#ifndef __NASAL_LIST_CPP__
+#define __NASAL_LIST_CPP__
+
+nasal_list::nasal_list()
+{
+	;
+}
+nasal_list::~nasal_list()
+{
+	;
+}
+nasal_list& nasal_list::operator=(const nasal_list &temp)
+{
+	return *this;
+}
+
+#endif
