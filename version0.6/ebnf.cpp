fa284f544cfc4f65e3c86db874877b72bd6cf702
Valk Richard Li <48872266+ValKmjolnir@users.noreply.github.com>
2019-08-23 18:11:13 +0800

    ebnf

---
+++ version0.6/ebnf.cpp
@@ -0,0 +1,12 @@
+
+<identifier> ::= IDENTIFIER
+<number> ::= NUMBER
+<string> ::= STRING
+<null_list> ::= <[><]>
+<null_hash> ::= <{><}>
+call_func ::= <identifier> <(> <...> <)>
+<list> ::= <[> <...> <]>
+<hash> ::= <{> <...> <}>
+<definition> ::= <var> <identifier> <=> <...> <;>
+<assignment> ::= <identifier> <=> <...> <;>
+<function> ::= <func><(> <...> <)> <{> <...> <}>
