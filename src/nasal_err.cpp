bce8148f60f3d9330b5063fad03df3cdbfcc1cbe
ValKmjolnir <lhk101lhk101@qq.com>
2023-07-10 00:11:46 +0800

    :sparkles: update

--- src/nasal_err.cpp
+++ src/nasal_err.cpp
@@ -13,7 +13,7 @@ std::ostream& back_white(std::ostream& s) {
 #ifdef _WIN32
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0xf0);
 #else
-    s<<"\033[7m";
+    s << "\033[7m";
 #endif
     return s;
 }
@@ -22,7 +22,7 @@ std::ostream& red(std::ostream& s) {
 #ifdef _WIN32
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x0c);
 #else
-    s<<"\033[91;1m";
+    s << "\033[91;1m";
 #endif
     return s;
 }
@@ -31,7 +31,7 @@ std::ostream& cyan(std::ostream& s) {
 #ifdef _WIN32
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x03);
 #else
-    s<<"\033[36;1m";
+    s << "\033[36;1m";
 #endif
     return s;
 }
@@ -40,7 +40,7 @@ std::ostream& orange(std::ostream& s) {
 #ifdef _WIN32
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x0e);
 #else
-    s<<"\033[93;1m";
+    s << "\033[93;1m";
 #endif
     return s;
 }
@@ -49,7 +49,7 @@ std::ostream& white(std::ostream& s) {
 #ifdef _WIN32
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x0f);
 #else
-    s<<"\033[0m\033[1m";
+    s << "\033[0m\033[1m";
 #endif
     return s;
 }
@@ -58,7 +58,7 @@ std::ostream& reset(std::ostream& s) {
 #ifdef _WIN32
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), reset_ter_color.scr.wAttributes);
 #else
-    s<<"\033[0m";
+    s << "\033[0m";
 #endif
     return s;
 }
@@ -73,7 +73,7 @@ void flstream::load(const std::string& f) {
     res.clear();
     std::ifstream in(f, std::ios::binary);
     if (in.fail()) {
-        std::cerr<<red<<"src: "<<reset<<"cannot open <"<<f<<">\n";
+        std::cerr << red << "src: " << reset << "cannot open <" << f << ">\n";
         std::exit(1);
     }
     
@@ -85,22 +85,22 @@ void flstream::load(const std::string& f) {
 }
 
 void error::fatal(const std::string& stage, const std::string& info) {
-    std::cerr<<red<<stage<<": "<<white<<info<<reset<<"\n";
+    std::cerr << red << stage << ": " << white << info << reset << "\n";
     if (file.length()) {
-        std::cerr<<cyan<<" --> "<<red<<file<<reset<<"\n\n";
+        std::cerr << cyan << " --> " << red << file << reset << "\n\n";
     } else {
-        std::cerr<<reset<<"\n";
+        std::cerr << reset << "\n";
     }
     std::exit(1);
 }
 
 void error::err(const std::string& stage, const std::string& info) {
     ++cnt;
-    std::cerr<<red<<stage<<": "<<white<<info<<reset<<"\n";
+    std::cerr << red << stage << ": " << white << info << reset << "\n";
     if (file.length()) {
-        std::cerr<<cyan<<" --> "<<red<<file<<reset<<"\n\n";
+        std::cerr << cyan << " --> " << red << file << reset << "\n\n";
     } else {
-        std::cerr<<reset<<"\n";
+        std::cerr << reset << "\n";
     }
 }
 
@@ -112,8 +112,9 @@ void error::err(
     ++cnt;
 
     std::cerr
-    <<red<<stage<<": "<<white<<info<<reset<<"\n"<<cyan<<"  --> "
-    <<red<<loc.file<<":"<<loc.begin_line<<":"<<loc.begin_column+1<<reset<<"\n";
+    << red << stage << ": " << white << info << reset << "\n" << cyan << "  --> "
+    << red << loc.file << ":" << loc.begin_line << ":" << loc.begin_column+1
+    << reset << "\n";
 
     const usize maxlen = std::to_string(loc.end_line).length();
     const std::string iden = identation(maxlen);
@@ -125,7 +126,8 @@ void error::err(
 
         if (loc.begin_line<line && line<loc.end_line) {
             if (line==loc.begin_line+1) {
-                std::cerr<<cyan<<iden<<" | "<<reset<<"...\n"<<cyan<<iden<<" | "<<reset<<"\n";
+                std::cerr << cyan << iden << " | " << reset << "...\n"
+                << cyan << iden << " | " << reset << "\n";
             }
             continue;
         }
@@ -136,37 +138,37 @@ void error::err(
         }
 
         const std::string& code=res[line-1];
-        std::cerr<<cyan<<leftpad(line, maxlen)<<" | "<<reset<<code<<"\n";
+        std::cerr << cyan << leftpad(line, maxlen) << " | " << reset << code << "\n";
         // output underline
-        std::cerr<<cyan<<iden<<" | "<<reset;
+        std::cerr << cyan << iden << " | " << reset;
         if (loc.begin_line==loc.end_line) {
-            for(u32 i=0; i<loc.begin_column; ++i) {
-                std::cerr<<char(" \t"[code[i]=='\t']);
+            for(u32 i = 0; i<loc.begin_column; ++i) {
+                std::cerr << char(" \t"[code[i]=='\t']);
             }
-            for(u32 i=loc.begin_column ;i<loc.end_column; ++i) {
-                std::cerr<<red<<(code[i]=='\t'?"^^^^":"^")<<reset;
+            for(u32 i = loc.begin_column ;i<loc.end_column; ++i) {
+                std::cerr << red << (code[i]=='\t'? "^^^^":"^") << reset;
             }
         } else if (line==loc.begin_line) {
-            for(u32 i=0; i<loc.begin_column; ++i) {
-                std::cerr<<char(" \t"[code[i]=='\t']);
+            for(u32 i = 0; i<loc.begin_column; ++i) {
+                std::cerr << char(" \t"[code[i]=='\t']);
             }
-            for(u32 i=loc.begin_column; i<code.size(); ++i) {
-                std::cerr<<red<<(code[i]=='\t'?"^^^^":"^")<<reset;
+            for(u32 i = loc.begin_column; i<code.size(); ++i) {
+                std::cerr << red << (code[i]=='\t'? "^^^^":"^") << reset;
             }
         } else if (loc.begin_line<line && line<loc.end_line) {
-            for(u32 i=0; i<code.size(); ++i) {
-                std::cerr<<red<<(code[i]=='\t'?"^^^^":"^");
+            for(u32 i = 0; i<code.size(); ++i) {
+                std::cerr << red << (code[i]=='\t'? "^^^^":"^");
             }
         } else {
-            for(u32 i=0; i<loc.end_column; ++i) {
-                std::cerr<<red<<(code[i]=='\t'?"^^^^":"^");
+            for(u32 i = 0; i<loc.end_column; ++i) {
+                std::cerr << red << (code[i]=='\t'? "^^^^":"^");
             }
         }
         if (line==loc.end_line) {
-            std::cerr<<reset;
+            std::cerr << reset;
         } else {
-            std::cerr<<reset<<"\n";
+            std::cerr << reset << "\n";
         }
     }
-    std::cerr<<"\n\n";
+    std::cerr << "\n\n";
 }
