1c03b702191cad03fe959a7eff5c468621de41af
ValKmjolnir <lhk101lhk101@qq.com>
2023-11-23 00:20:52 +0800

    :zap: replace vm_type comparison with .is_xxx

--- src/dylib_lib.cpp
+++ src/dylib_lib.cpp
@@ -15,7 +15,7 @@ void dynamic_library_destructor(void* pointer) {
 
 var builtin_dlopen(context* ctx, gc* ngc) {
     auto dlname = ctx->localr[1];
-    if (dlname.type!=vm_type::vm_str) {
+    if (!dlname.is_str()) {
         return nas_err("dylib::dlopen", "\"libname\" must be string");
     }
 
@@ -43,7 +43,7 @@ var builtin_dlopen(context* ctx, gc* ngc) {
         );
     }
     auto return_hash = ngc->temp = ngc->alloc(vm_type::vm_hash);
-    auto library_object = ngc->alloc(vm_type::vm_obj);
+    auto library_object = ngc->alloc(vm_type::vm_ghost);
     library_object.ghost().set(
         dynamic_library_type_name,
         dynamic_library_destructor,
@@ -73,7 +73,7 @@ var builtin_dlopen(context* ctx, gc* ngc) {
     }
     for(u32 i = 0; table[i].name; ++i) {
         auto function_pointer = reinterpret_cast<void*>(table[i].fd);
-        auto function_object = ngc->alloc(vm_type::vm_obj);
+        auto function_object = ngc->alloc(vm_type::vm_ghost);
         function_object.ghost().set(
             function_address_type_name,
             nullptr,
