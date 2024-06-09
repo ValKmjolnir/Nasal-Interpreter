a5dc725010ebb42c58074bdaa4161324d682ccb6
ValKmjolnir <lhk101lhk101@qq.com>
2024-06-10 00:30:30 +0800

    :sparkles: add native functions of subprocess

--- src/nasal_codegen.h
+++ src/nasal_codegen.h
@@ -60,7 +60,9 @@ private:
         "__pipe", "__fork", "__waitpid", "__chdir",
         "__environ", "__getcwd", "__getenv",
         // subprocess
-        "__subprocess_run"
+        "__subprocess_popen",
+        "__subprocess_pclose",
+        "__subprocess_read_stdout"
     };
 
     // file mapper for file -> index
