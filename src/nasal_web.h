8e7074fd2500868e60d2944f474740a140d56f0a
Sidi Liang <1467329765@qq.com>
2024-11-09 13:40:45 +0800

    [web] Attempt to add timeouts for REPL (not working properly, it works in some cases but causes a segfault, and in other cases the timeouts were ignored)

--- src/nasal_web.h
+++ src/nasal_web.h
@@ -23,6 +23,7 @@ NASAL_EXPORT const char* nasal_get_error(void* context);
 // REPL 
 NASAL_EXPORT void* nasal_repl_init();
 NASAL_EXPORT void nasal_repl_cleanup(void* repl_context);
+NASAL_EXPORT void nasal_repl_set_timeout(void* repl_context, int seconds);
 NASAL_EXPORT const char* nasal_repl_eval(void* repl_context, const char* line);
 NASAL_EXPORT int nasal_repl_is_complete(void* repl_context, const char* line);
 NASAL_EXPORT const char* nasal_repl_get_version();
