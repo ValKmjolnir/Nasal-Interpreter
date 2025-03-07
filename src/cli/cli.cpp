a7a2a0a3698c3b42e62bc29d0e34d52b94d631f7
ValKmjolnir <lhk101lhk101@qq.com>
2025-03-08 01:45:54 +0800

    :memo: fix typo

--- src/cli/cli.cpp
+++ src/cli/cli.cpp
@@ -105,7 +105,7 @@ std::ostream& logo(std::ostream& out) {
     << " - http://fgprc.org\n"
     << " - http://fgprc.org.cn\n"
     << "\n"
-    << "input <nasal-format -h> to get help.\n\n";
+    << "input <nasal -h> to get help.\n\n";
     return out;
 }
 
